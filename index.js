7846b7ac7c745b61a062b6041425a227dc2a23a7
snegur04ka <barte977@mail.ru>
2022-12-18 21:55:19 +0600

    povidlo

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const arr = ["Груша", "Яблоко", "Повидло", "Завали", "Ебало", "Быдло"];
 const ul = document.querySelector("ul")
-const btn = document.querySelector("button")
+const btn = document.querySelector(".btn")
 let i = 0;
-btn,addEventListener("click", () => {
+btn.addEventListener("click", () => {
         if (i >= arr.length) {
         
         } else {
                 ul.innerHTML += `<li>${arr[i]}</li>`
                 i++
         }
+})
+
+const box = document.querySelector(".box")
+// const blueBtn = document.querySelector(".changeToBlue")
+const redBtn = document.querySelector(".changeToRed")
+
+// blueBtn.addEventListener("click", () => {
+//     box.className = "blue";
+// })
+redBtn.addEventListener("click", () => {
+    if(box.className === "box") {
+        box.className = "blue"
+    } else if(box.className === "blue") {
+        box.className = "purple"
+    } else {
+        box.className = "box"
+    }
 })
