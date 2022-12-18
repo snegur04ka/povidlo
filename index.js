09fa5a803ae716ba1b008daaff23264c15c870cb
snegur04ka <barte977@mail.ru>
2022-12-18 20:56:49 +0600

    povidlo

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const arr = ["Груша", "Яблоко", "Повидло", "Завали", "Ебало", "Быдло"];
 const ul = document.querySelector("ul")
 const btn = document.querySelector("button")
-let i = -1;
+let i = 0;
 btn,addEventListener("click", () => {
-        i++
-        ul.innerHTML += `<li>${arr[i]}</li>`
+        if (i >= arr.length) {
+        
+        } else {
+                ul.innerHTML += `<li>${arr[i]}</li>`
+                i++
+        }
 })
